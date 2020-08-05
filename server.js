427e81674d564a42e43d7500ea002ea6b4eaf216
Michael Pomatto <mike@pomatto.com>
2020-08-04 21:46:12 -0400

    Updated Files

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/', (request, response) => {
-  response.send('Hello Express');
+  response.render('pages/index',{ pageTitle: 'Welcome'});
+});
+
+app.get('/speakers',(request, response) => {
+    response.sendFile(path.join(__dirname, './static/speakers.html'))
 });
 
 app.listen(port, () => {
-  console.log('Express server listening on port ${port}');
+  console.log('Express server listening on port 3000');
 });
