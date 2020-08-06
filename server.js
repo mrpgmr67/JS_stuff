6df42471ed78c8e4da31d7a081ce9e94894f4263
Michael Pomatto <mike@pomatto.com>
2020-08-06 04:36:05 -0400

    Updated with Chapter 4 information

--- server.js
+++ server.js
@@ -1,20 +1,54 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakerService = new SpeakerService('./data/speakers.json');
+
+const routes = require('./routes');
+const { response, request } = require('express');
+
 const app = express();
 const port = 3000;
 
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['dadridurl7i3', 'faielsxcvi212'],
+  })
+);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
+
+app.locals.siteName = 'ROUX Meetups';
+
 app.use(express.static(path.join(__dirname, './static')));
-
-app.get('/', (request, response) => {
-  response.render('pages/index',{ pageTitle: 'Welcome'});
+app.use(async(request, response, next) => {
+  try {
+    const names = await speakerService.getNames();
+    response.locals.speakerNames = names;
+    console.log(response.locals);
+    return next();
+  } catch(err) {
+    return next(err);
+  }
 });
 
-app.get('/speakers',(request, response) => {
-    response.sendFile(path.join(__dirname, './static/speakers.html'))
-});
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakerService,
+  })
+);
 
 app.listen(port, () => {
-  console.log('Express server listening on port 3000');
+  // eslint-disable-next-line no-console
+  console.log(`Express server listening on port ${port}`);
 });
